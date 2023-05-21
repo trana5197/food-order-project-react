793dd37c93ffca0be5f8f4aed5f0e44b6a91f458
rana <trana51997@gmail.com>
2023-05-21 17:22:09 -0500

    Started Files plus Main Header
    Contains all the started files but only starting components for main header

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1>HELLO WORLD</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/MainHeader.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+const MainHeader = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>ReactMeals</h1>
+      </div>
+      {/* <></> TODO: This will have cart bar */}
+    </Fragment>
+  );
+};
+
+export default MainHeader;
