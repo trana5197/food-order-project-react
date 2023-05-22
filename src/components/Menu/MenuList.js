6e9469fbed5d59edfcb92608053601d7fea96e5d
rana <trana51997@gmail.com>
2023-05-22 17:05:38 -0500

    UI Complete

--- src/components/Menu/MenuList.js
+++ src/components/Menu/MenuList.js
@@ -4,7 +4,7 @@ import classes from "./MenuList.module.css";
 
 const MenuList = ({ meal }) => {
   return (
-    <li className={classes.list}>
+    <li className={`margin-left-md margin-bottom-sm ${classes.list}`}>
       <div className={classes.meal}>
         <p className={classes.name}>{meal.name}</p>
         <p className={classes.desc}>{meal.desc}</p>
